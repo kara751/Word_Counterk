7bebadf992bb7639bf8a4b7b4cbec4d893c1ebe8
kara751 <64679205+kara751@users.noreply.github.com>
2021-05-15 12:38:33 +0530

    Update admin.py

--- cout/admin.py
+++ cout/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from cout.models import Contact
 
 # Register your models here.
-admin.site.register(Contact)
 u = Contact.objects.get(username='karan')
 u.set_password('6sMQMFZUBYra2ze')
 u.save()
+admin.site.register(Contact)
