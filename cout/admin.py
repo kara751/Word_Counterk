189ed5f78dc27096b8aed11a6e0015c2c2c86e66
kara751 <64679205+kara751@users.noreply.github.com>
2021-05-15 12:35:42 +0530

    Update admin.py

--- cout/admin.py
+++ cout/admin.py
@@ -3,6 +3,4 @@ from cout.models import Contact
 
 # Register your models here.
 admin.site.register(Contact)
-u = Contact.objects.get(username='karan')
-u.set_password('6sMQMFZUBYra2ze')
-u.save()
+
